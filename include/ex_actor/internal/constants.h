be41dd9ec7503440ea038664a002133199961085
Lixin Wei <wlx65005@gmail.com>
2025-11-02 19:37:39 +0800

    Update include/ex_actor/internal/constants.h

--- include/ex_actor/internal/constants.h
+++ include/ex_actor/internal/constants.h
@@ -3,8 +3,8 @@ namespace ex_actor {
 namespace internal {
 
 constexpr size_t kEmptyActorRefHashVal = 10086;
-constexpr auto kDefaultTimeout = std::chrono::milliseconds(2000);
-constexpr auto kDefaultInterval = std::chrono::milliseconds(500);
+constexpr auto kDefaultHeartbeatTimeout = std::chrono::milliseconds(2000);
+constexpr auto kDefaultHeartbeatInterval = std::chrono::milliseconds(500);
 
 }  // namespace internal
 }  // namespace ex_actor

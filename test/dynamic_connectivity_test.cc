32a67c182af1b48ffca4e297f0342023ae32b978
valueote <113183547+valueote@users.noreply.github.com>
2026-02-09 23:14:00 +0800

    [tests] Add chain tests for dynamic_connectivity

--- test/dynamic_connectivity_test.cc
+++ test/dynamic_connectivity_test.cc
@@ -1,13 +1,12 @@
-#include <cassert>
 #include <chrono>
 #include <cstdint>
 #include <cstdlib>
 #include <random>
+#include <string>
 #include <thread>
+#include <vector>
 
 #include "ex_actor/api.h"
-#include "ex_actor/internal/actor_registry.h"
-#include "ex_actor/internal/logging.h"
 #include "spdlog/fmt/bundled/format.h"
 
 using std::chrono::milliseconds;
@@ -37,65 +36,108 @@ class Echoer {
 };
 EXA_REMOTE(&Echoer::Create, &Echoer::Echo, &Echoer::Proxy, &Echoer::ProxyTwoActor);
 
-class StarTest {
+enum class TestType : uint8_t {
+  kStar,
+  kChain,
+};
+
+class DynamicConnectivityTest {
  public:
-  StarTest(uint32_t this_node_id, uint32_t cluster_size) : this_node_id_(this_node_id), cluster_size_(cluster_size) {};
-  void test() {
-    ex_actor::ClusterConfig config;
-    auto base_port = 5000;
-    config.this_node = {.node_id = this_node_id_,
-                        .address = fmt::format("tcp://127.0.0.1:{}", base_port + this_node_id_)};
-    config.network_config = {.heartbeat_timeout = std::chrono::milliseconds {1000},
-                             .gossip_interval = std::chrono::milliseconds {100},
-                             .gossip_fanout = cluster_size_ / 2};
+  DynamicConnectivityTest(uint32_t this_node_id, uint32_t cluster_size, TestType type)
+      : this_node_id_(this_node_id), cluster_size_(cluster_size), type_(type) {}
 
-    std::random_device device;
-    auto random_generator = std::mt19937(device());
-    std::uniform_int_distribution<uint32_t> dist(0, cluster_size_ - 1);
-    uint32_t target_node_id = dist(random_generator);
-    if (this_node_id_ != 0) {
-      config.contact_node = {.node_id = 0, .address = fmt::format("tcp://127.0.0.1:{}", base_port)};
-    }
+  void Test() {
+    auto config = BuildConfig();
     ex_actor::Init(1, config);
-    auto [connected] =
-        stdexec::sync_wait(ex_actor::WaitNodeAlive(target_node_id, std::chrono::milliseconds {3000})).value();
-    // Sync all  nodes
+    CheckConnectivityAndCallMethod();
+    std::this_thread::sleep_for(std::chrono::milliseconds {1500});
+    ex_actor::Shutdown();
+  }
 
-    EXA_THROW_CHECK(connected) << fmt::format("Error: this node {} can't connected to node {}/n", this_node_id_,
+ private:
+  ex_actor::ClusterConfig BuildConfig() const {
+    ex_actor::ClusterConfig config;
+    constexpr uint32_t kBasePort = 5000;
+    config.this_node = {.node_id = this_node_id_,
+                        .address = fmt::format("tcp://127.0.0.1:{}", kBasePort + this_node_id_)};
+    config.network_config = {
+        .heartbeat_timeout = std::chrono::milliseconds {1000},
+        .gossip_interval = std::chrono::milliseconds {100},
+        .gossip_fanout = cluster_size_ > 1 ? cluster_size_ / 2 : 1,
+    };
+
+    if (this_node_id_ != 0) {
+      uint32_t contact_node_id = 0;
+      if (type_ == TestType::kChain) {
+        contact_node_id = this_node_id_ - 1;
+      }
+      config.contact_node = {.node_id = contact_node_id,
+                             .address = fmt::format("tcp://127.0.0.1:{}", kBasePort + contact_node_id)};
+    }
+    return config;
+  }
+
+  void CheckConnectivityAndCallMethod() const {
+    uint32_t target_node_id;
+    if (type_ == TestType::kStar) {
+      std::random_device device;
+      std::mt19937 random_generator(device());
+      std::uniform_int_distribution<uint32_t> dist(0, cluster_size_ - 1);
+      target_node_id = dist(random_generator);
+    } else {
+      target_node_id = cluster_size_ - 1 - this_node_id_;
+      if (target_node_id == this_node_id_) {
+        target_node_id = (this_node_id_ + 1) % cluster_size_;
+      }
+    }
+
+    auto [connected] =
+        stdexec::sync_wait(ex_actor::WaitNodeAlive(target_node_id, std::chrono::milliseconds {6000})).value();
+    EXA_THROW_CHECK(connected) << fmt::format("Error: node {} can't connected to node {}", this_node_id_,
                                               target_node_id);
 
+    std::string str {"Hello"};
     auto actor_creation_sender = ex_actor::Spawn<Echoer>();
     auto [actor_ref] = stdexec::sync_wait(actor_creation_sender).value();
-    std::string str {"Hello"};
     auto actor_method_calling_sender = actor_ref.Send<&Echoer::Echo>(str);
     auto [method_calling_return_val] = stdexec::sync_wait(std::move(actor_method_calling_sender)).value();
     EXA_THROW_CHECK(method_calling_return_val == str) << fmt::format("Error: local method calling error");
 
     auto remote_actor_creation_sender = ex_actor::Spawn<Echoer, &Echoer::Create>({.node_id = target_node_id});
     auto [remote_actor_ref] = stdexec::sync_wait(std::move(remote_actor_creation_sender)).value();
-    auto remote_actor_calling_sender = actor_ref.Send<&Echoer::Echo>(str);
+    auto remote_actor_calling_sender = remote_actor_ref.Send<&Echoer::Echo>(str);
     auto [remote_method_calling_return_val] = stdexec::sync_wait(std::move(remote_actor_calling_sender)).value();
-    EXA_THROW_CHECK(remote_method_calling_return_val == str) << fmt::format("Error: local method calling error");
-
-    // Sync all  nodes
-    std::this_thread::sleep_for(std::chrono::milliseconds {1500});
-    ex_actor::Shutdown();
+    EXA_THROW_CHECK(remote_method_calling_return_val == str) << fmt::format("Error: remote method calling error");
   }
 
- private:
   uint32_t this_node_id_;
   uint32_t cluster_size_;
+  TestType type_;
 };
 
 int main(int argc, char* argv[]) {
-  if (argc < 2) {
-    return 1;
+  if (argc < 4) {
+    return -1;
   }
 
   uint32_t cluster_size = std::atoi(argv[1]);
   uint32_t this_node_id = std::atoi(argv[2]);
-  StarTest star_test(this_node_id, cluster_size);
-  star_test.test();
+  if (cluster_size == 0 || this_node_id >= cluster_size) {
+    return 1;
+  }
+
+  TestType type;
+  std::string type_str = argv[3];
+  if (type_str == "star") {
+    type = TestType::kStar;
+  } else if (type_str == "chain") {
+    type = TestType::kChain;
+  } else {
+    return -1;
+  }
+
+  DynamicConnectivityTest dynamic_connectivity_test(this_node_id, cluster_size, type);
+  dynamic_connectivity_test.Test();
 
   return 0;
 }
--- test/dynamic_connectivity_test.py
+++ test/dynamic_connectivity_test.py
@@ -4,10 +4,13 @@ import subprocess
 import sys
 import time
 
+CLUSTER_SIZE = 8
+
 
 def clear_nodes(node_list):
     for node in node_list:
-        node.kill()
+        if node.poll() is None:
+            node.kill()
     for node in node_list:
         node.wait()
 
@@ -17,22 +20,29 @@ def check_nodes(node_list):
     for node in node_list:
         if node.poll() is None:
             clear_nodes(node_list)
-            raise RuntimeError("There's node in the cluster timeout")
-    time.sleep(5)
+            raise RuntimeError("There is node in the cluster timeout")
     for node in node_list:
         if node.returncode != 0:
             clear_nodes(node_list)
-            raise RuntimeError("There's error in the cluster")
+            raise RuntimeError("There is error in the cluster")
 
 
-def star_test(bin):
-    cluster_size = 8
+def run_test(bin, test_type):
     node_list = []
-    for node_id in range(cluster_size):
-        node_list.append(subprocess.Popen([bin, str(cluster_size), str(node_id)]))
+    for node_id in range(CLUSTER_SIZE):
+        node_list.append(
+            subprocess.Popen([bin, str(CLUSTER_SIZE), str(node_id), test_type])
+        )
     check_nodes(node_list)
 
 
 if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        raise RuntimeError("usage: dynamic_connectivity_test.py <bin> <star|chain>")
+
     bin = sys.argv[1]
-    star_test(bin)
+    type = sys.argv[2]
+    if type not in {"star", "chain"}:
+        raise RuntimeError(f"unsupported type: {type}")
+
+    run_test(bin, type)

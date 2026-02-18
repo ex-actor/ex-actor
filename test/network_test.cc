1503110470ae27ab020371c92140942e69a7da9e
valueote <113183547+valueote@users.noreply.github.com>
2026-02-18 19:17:37 +0800

    [tests] Add tests for duplicate nodes case

--- test/network_test.cc
+++ test/network_test.cc
@@ -1,8 +1,15 @@
 #include "ex_actor/internal/network.h"
 
 #include <atomic>
+#include <barrier>
+#include <chrono>
+#include <cstdint>
+#include <exception>
 #include <thread>
+#include <utility>
+#include <vector>
 
+#include <gmock/gmock-matchers.h>
 #include <gtest/gtest.h>
 #include <spdlog/spdlog.h>
 
@@ -56,6 +63,14 @@ TEST(NetworkTest, NodeInfoManagerBasicTest) {
   manager.DeactivateNode(2);
   EXPECT_TRUE(manager.Connected(1));
   EXPECT_TRUE(manager.Connected(2));
+  EXPECT_TRUE(manager.GenerateGossipMessage().empty());
+
+  manager.Add(4, {.liveness = Liveness::kAlive, .last_seen = 100, .address = "tcp://127.0.0.1:7301"});
+
+  EXPECT_THAT([&manager]() -> void { (void)manager.Connected(4, "tcp://127.0.0.1:7302"); },
+              testing::Throws<std::exception>(testing::Property(
+                  &std::exception::what,
+                  testing::HasSubstr("Nodes with the same node ID but different addresses exist in the cluster."))));
 }
 
 TEST(NetworkTest, NodeInfoManagerWaiterTest) {
@@ -76,54 +91,10 @@ TEST(NetworkTest, NodeInfoManagerWaiterTest) {
   spawn_waiter(4);
   spawn_waiter(4);
 
-  std::jthread notifier([&]() {
-    manager.NotifyWaiters(4);
-    std::this_thread::sleep_for(std::chrono::milliseconds {1});
-  });
-
+  manager.NotifyWaiters(4);
   stdexec::sync_wait(waiter_scope.on_empty());
 
   EXPECT_EQ(woke_count.load(std::memory_order_relaxed), 2);
-}
-
-TEST(NetworkTest, MessageBrokerBasicTest) {
-  auto test_once = []() {
-    std::vector<ex_actor::NodeInfo> node_list = {{.node_id = 0, .address = "tcp://127.0.0.1:5201"},
-                                                 {.node_id = 1, .address = "tcp://127.0.0.1:5202"},
-                                                 {.node_id = 2, .address = "tcp://127.0.0.1:5203"}};
-
-    auto node_main = [&node_list](uint32_t node_id) {
-      ex_actor::internal::util::SetThreadName("node_" + std::to_string(node_id));
-      ex_actor::ClusterConfig config {.network_config {.gossip_interval = std::chrono::milliseconds {50}}};
-      config.this_node = node_list.at(node_id);
-      config.contact_node = node_list.at((node_id + node_list.size() - 1) % node_list.size());
-      ex_actor::internal::network::MessageBroker message_broker(
-          config, [&message_broker](uint64_t received_request_id, ByteBufferType data) {
-            message_broker.ReplyRequest(received_request_id, std::move(data));
-          });
-      uint32_t to_node_id = (node_id + 1) % node_list.size();
-      // Waiting all the nodes find its contact node
-      std::this_thread::sleep_for(std::chrono::milliseconds {500});
-      exec::async_scope scope;
-      for (int i = 0; i < 5; ++i) {
-        scope.spawn(message_broker.SendRequest(to_node_id, ByteBufferType(std::to_string(node_id))) |
-                    stdexec::then([node_id](ByteBufferType data) {
-                      std::string data_str(static_cast<char*>(data.data()), data.size());
-                      logging::Info("got response data, node id: {}, data: {}", node_id, data_str);
-                      ASSERT_EQ(data_str, std::to_string(node_id));
-                    }));
-      }
-      stdexec::sync_wait(scope.on_empty());
-    };
-    std::jthread node_0(node_main, 0);
-    std::jthread node_1(node_main, 1);
-    std::jthread node_2(node_main, 2);
-  };
-
-  for (int i = 0; i < 10; ++i) {
-    logging::Info("test once, {}", i);
-    test_once();
-  }
 }
 
 TEST(NetworkTest, MessageBrokerClusterConfigTest) {
@@ -131,7 +102,8 @@ TEST(NetworkTest, MessageBrokerClusterConfigTest) {
                                                {.node_id = 1, .address = "tcp://127.0.0.1:6202"},
                                                {.node_id = 2, .address = "tcp://127.0.0.1:6203"}};
   auto test_once = [&node_list]() {
-    auto node_main = [](const std::vector<ex_actor::NodeInfo>& node_list, uint32_t node_id) {
+    std::barrier bar {3};
+    auto node_main = [&bar](const std::vector<ex_actor::NodeInfo>& node_list, uint32_t node_id) {
       ex_actor::internal::util::SetThreadName("node_" + std::to_string(node_id));
       ex_actor::ClusterConfig config {.network_config {.gossip_interval = std::chrono::milliseconds {50}}};
       config.this_node = node_list.at(node_id);
@@ -144,7 +116,7 @@ TEST(NetworkTest, MessageBrokerClusterConfigTest) {
           });
       uint32_t to_node_id = (node_id + 1) % node_list.size();
       // Waiting all the nodes find its contact node
-      std::this_thread::sleep_for(std::chrono::milliseconds {500});
+      stdexec::sync_wait(message_broker.WaitNodeAlive(to_node_id, std::chrono::milliseconds {500}));
       exec::async_scope scope;
       for (int i = 0; i < 5; ++i) {
         scope.spawn(message_broker.SendRequest(to_node_id, ByteBufferType(std::to_string(node_id))) |
@@ -155,6 +127,7 @@ TEST(NetworkTest, MessageBrokerClusterConfigTest) {
                     }));
       }
       stdexec::sync_wait(scope.on_empty());
+      bar.arrive_and_wait();
     };
 
     std::jthread node_0(node_main, node_list, 0);
@@ -166,4 +139,62 @@ TEST(NetworkTest, MessageBrokerClusterConfigTest) {
     logging::Info("test once, {}", i);
     test_once();
   }
+}
+
+TEST(NetworkTest, MessageBrokerDuplicateContactNodeTest) {
+  auto request_handler = [](uint64_t /*received_request_id*/, ByteBufferType /*data*/) {};
+
+  auto make_cluster_config = [](std::string this_address, std::string contact_address) {
+    ex_actor::ClusterConfig config;
+    config.this_node = {.node_id = 0, .address = std::move(this_address)};
+    config.contact_node = {.node_id = 0, .address = std::move(contact_address)};
+    return config;
+  };
+
+  auto duplicate_node_id_and_addr = make_cluster_config("tcp://127.0.0.1:7201", "tcp://127.0.0.1:7201");
+  EXPECT_THAT(
+      [&]() -> void { (void)ex_actor::internal::network::MessageBroker(duplicate_node_id_and_addr, request_handler); },
+      testing::Throws<std::exception>(testing::Property(
+          &std::exception::what,
+          testing::HasSubstr("The local node has the same node ID or address as the contact node."))));
+
+  auto duplicate_node_id_different_addr = make_cluster_config("tcp://127.0.0.1:7202", "tcp://127.0.0.1:7203");
+  EXPECT_THAT(
+      [&]() -> void {
+        (void)ex_actor::internal::network::MessageBroker(duplicate_node_id_different_addr, request_handler);
+      },
+      testing::Throws<std::exception>(testing::Property(
+          &std::exception::what, testing::HasSubstr("The local node has the same node ID or address as the "
+                                                    "contact node."))));
+}
+
+TEST(NetworkTest, MessageBrokerDuplicateClusterNodesTest) {
+  auto request_handler = [](uint64_t /*received_request_id*/, ByteBufferType /*data*/) {};
+
+  std::vector<ex_actor::NodeInfo> nodes {ex_actor::NodeInfo {.node_id = 0, .address = "tcp://127.0.0.1:5000"},
+                                         ex_actor::NodeInfo {.node_id = 2, .address = "tcp://127.0.0.1:5001"},
+                                         ex_actor::NodeInfo {.node_id = 2, .address = "tcp://127.0.0.1:5002"},
+                                         ex_actor::NodeInfo {.node_id = 0, .address = "tcp://127.0.0.1:5003"}};
+
+  auto create_node = [&request_handler](ex_actor::NodeInfo this_node, ex_actor::NodeInfo contact_node = {}) {
+    ex_actor::ClusterConfig cluster_config {.this_node = std::move(this_node), .contact_node = std::move(contact_node)};
+    ex_actor::internal::network::MessageBroker message_broker(cluster_config, request_handler);
+    std::this_thread::sleep_for(std::chrono::milliseconds {1000});
+  };
+
+  auto duplicate_node_test = [&]() {
+    std::jthread node_0(create_node, nodes.at(0));
+    std::jthread node_1(create_node, nodes.at(1), nodes.at(0));
+    std::jthread node_2(create_node, nodes.at(2), nodes.at(0));
+  };
+
+  auto duplicate_node_forward_test = [&]() {
+    std::jthread node_0(create_node, nodes.at(0));
+    std::jthread node_1(create_node, nodes.at(1), nodes.at(0));
+    // Let the node 1 forward this duplicate node to node 0
+    std::jthread node_2(create_node, nodes.at(3), nodes.at(1));
+  };
+
+  EXPECT_DEATH(duplicate_node_forward_test(),
+               "Nodes with the same node ID but different addresses exist in the cluster");
 }
